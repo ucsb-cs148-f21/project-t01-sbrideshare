39e8f1298a24d7d9baf73fb382ac17557be1bc86
Heather Dinh <heatherdinh11@gmail.com>
2021-10-04 21:15:27 -0700

    HD - Hello World in React!

---
+++ frontend/app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hello World!
+        </h1>
+        <p>
+          Heather Dinh | SBRideShare | CS148
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
