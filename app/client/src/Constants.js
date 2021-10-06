6bae39776723c7504fab3d337429aea15bbb4526
LucienLuc <lucienluc@umail.ucsb.edu>
2021-10-05 17:54:25 -0700

    Comment issue with heroku and NODE_ENV

--- app/client/src/Constants.js
+++ app/client/src/Constants.js
@@ -1,10 +1,15 @@
 var url = ""
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    url = "http://localhost:9000"
-} else {
-    url = "https://sb-rideshare-backend.herokuapp.com"
-}
-
+/* 
+ISSUE: Heroku doesn't seem to set NODE_ENV to production. Logging process.env.NODE_ENV outputs "development" 
+Manually change url for now
+*/
+// if (process.env.NODE_ENV === 'production') {
+//     url = "https://sb-rideshare-backend.herokuapp.com"
+// } else {
+//     url = "http://localhost:9000"
+// }
+url = "https://sb-rideshare-backend.herokuapp.com"
+//url = "http://localhost:9000"
 
 export const API_URL = url
