eda2722cef07955017e1674b393537669ad3f9c0
Robert Gee <robertgee@ucsb.edu>
2021-10-06 20:11:12 -0700

    Adding Hello World app files

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './gaucho.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World!
+        </p>
+        <p>
+          UCSB Rideshare Project - Robert Gee
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
