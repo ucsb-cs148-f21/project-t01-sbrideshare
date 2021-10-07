ffcdc30c9dd05da7d31969963f8eb45e0678ea87
Heather Dinh <heatherdinh11@gmail.com>
2021-10-07 15:10:34 -0700

    fixing directories in React app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img width={300} src={"https://appstickers-cdn.appadvice.com/1443674037/839453796/1a2fbf419cc1f1f1332fad36a807a4a5-2.png"} alt="mapache" />
         <h1>
           Hello World!
         </h1>
