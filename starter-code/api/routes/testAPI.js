ebc225d2a26bbdd18015420f32ea7d4eb9dab302
LucienLuc <lucienluc@umail.ucsb.edu>
2021-10-11 18:34:10 -0700

    Express backend with MongoDB

---
+++ starter-code/api/routes/testAPI.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var router = express.Router();
+
+const Rides = require('../models/rides')
+
+router.get("/", function(req, res, next) {
+    res.send("API is working properly");
+});
+
+router.post("/testCreateRide", function(req, res, next) {
+    const body = req.body;
+    var new_rides_entry = new Rides(
+        {
+            location: body.location,
+            name: body.name,
+            date: Date.now(),
+            price: body.price,
+            phone: body.phone,
+            seats_available: body.seats_available
+        }
+    )
+
+    new_rides_entry.save(function(err) {
+        if (err){
+            console.log(err);
+        }
+    });
+
+    res.status(200).end();
+});
+
+module.exports = router;
