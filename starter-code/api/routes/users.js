cf53f921583296f0510d87e30cf50751d74f91ec
Heather Dinh <heatherdinh11@gmail.com>
2021-10-25 15:10:59 -0700

    update testing and api

--- starter-code/api/routes/users.js
+++ starter-code/api/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 //var ObjectId = require('mongoose').Types.ObjectId;
+const { body, validationResult } = require('express-validator');
 
 const Users = require('../models/users');
 
@@ -12,8 +13,29 @@ router.get("/", function(req, res, next) {
 
 });
 
-router.post("/",    
+router.post("/",  
+    body("full_name")
+        .exists().withMessage('full_name is required.').bail()
+        .notEmpty().withMessage('full_name is required.').bail(), 
+    body("given_name")
+        .exists().withMessage('given_name is required.').bail()
+        .notEmpty().withMessage('given_name is required.').bail(), 
+    body("family_name")
+        .exists().withMessage('family_name is required.').bail()
+        .notEmpty().withMessage('family_name is required.').bail(), 
+    body("_id")
+        .exists().withMessage('_id is required.').bail()
+        .notEmpty().withMessage('_id is required.').bail(), 
+    body("email")
+        .exists().withMessage('email is required.').bail()
+        .notEmpty().withMessage('email is required.').bail(), 
+        
     function(req, res, next) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+
         const body = req.body;
         Users.create({
             _id: body.id,
@@ -33,7 +55,8 @@ router.post("/",
                 return res.status(200).end();
             })
         }).catch((error) => {
-          console.log('User already in database. \n[', error.message, ']\n');
+          console.log(error.message);
+          return res.status(409).end();
         })
     }
 );
---
+++ starter-code/api/tests/testUsers.js
@@ -0,0 +1,220 @@
+const request = require('supertest');
+const mongoose = require('mongoose');
+const assert = require('assert');
+
+const app = require("../app");
+
+// wait until app connects to Mongo
+/*
+before(function (done) {
+  app.on("mongoConnected", function(){
+    done();
+  });
+}); */
+
+// destroy test DB after all tests and close connection
+after(function() {
+  mongoose.connection.db.dropDatabase().then(res => {
+    mongoose.connection.close()
+  })
+});
+
+// Can run basic get on server
+describe("GET /testAPI", function() {
+  it("it should has status code 200", function(done) {
+    request(app)
+      .get("/testAPI")
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
+
+// validation tests
+describe("POST /users validation", function() {
+
+    after(function(done) {
+      mongoose.connection.db.dropDatabase().then(res => {
+        done();
+      })
+    });
+
+    it("400 on missing _id", function(done) {
+        request(app)
+            .post("/users")
+            .send({
+                "full_name": "Joe Goldberg",
+                "given_name": "Joe", 
+                "family_name": "Goldberg",
+                "email": "joegoldber@ucsb.edu", 
+                "drives": [], 
+                "rides": [],
+                "history": [],
+            })
+            .set('Accept', 'application/json')
+            .expect(400)
+            .then(res => {
+            done();
+            })
+        });
+  
+    it("400 on missing full_name", function(done) {
+      request(app)
+        .post("/users")
+        .send({
+            "_id": "123",
+            "given_name": "Joe", 
+            "family_name": "Goldberg", 
+            "email": "joegoldber@ucsb.edu", 
+            "drives": [], 
+            "rides": [],
+            "history": [],
+        })
+        .set('Accept', 'application/json')
+        .expect(400)
+        .then(res => {
+          done();
+        })
+    });
+
+    it("400 on missing given_name", function(done) {
+        request(app)
+          .post("/users")
+          .send({
+              "_id": "123",
+              "full_name": "Joe Goldberg",
+              "family_name": "Goldberg", 
+              "email": "joegoldber@ucsb.edu", 
+              "drives": [], 
+              "rides": [],
+              "history": [],
+          })
+          .set('Accept', 'application/json')
+          .expect(400)
+          .then(res => {
+            done();
+          })
+      });
+
+    it("400 on missing family_name", function(done) {
+    request(app)
+        .post("/users")
+        .send({
+            "_id": "123",
+            "full_name": "Joe Goldberg",
+            "given_name": "Joe", 
+            "email": "joegoldber@ucsb.edu", 
+            "drives": [], 
+            "rides": [],
+            "history": [],
+        })
+        .set('Accept', 'application/json')
+        .expect(400)
+        .then(res => {
+        done();
+        })
+    });
+  
+    it("400 on missing email", function(done) {
+    request(app)
+        .post("/users")
+        .send({
+            "_id": "123",
+            "full_name": "Joe Goldberg",
+            "given_name": "Joe", 
+            "family_name": "Goldberg", 
+            "drives": [], 
+            "rides": [],
+            "history": [],
+        })
+        .set('Accept', 'application/json')
+        .expect(400)
+        .then(res => {
+        done();
+        })
+    });
+  
+    it("200 on valid inputs", function(done) {
+      request(app)
+        .post("/users")
+        .send({
+          "_id": "123",
+          "full_name": "Joe Goldberg",
+          "given_name": "Joe", 
+          "family_name": "Goldberg", 
+          "email": "joegoldberg@ucsb.edu",
+          "drives": [], 
+          "rides": [],
+          "history": []
+        })
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(res => {
+          done();
+        })
+    });
+  });
+  
+describe("POST /users", function() {
+  
+    after(function(done) {
+      mongoose.connection.db.dropDatabase().then(res => {
+        done();
+      })
+    });
+  
+    const data = {
+      _id: "123",
+      full_name: "Joe Goldberg",  
+      given_name: "Joe", 
+      family_name: "Goldberg", 
+      email: "joegoldber@ucsb.edu", 
+      drives: [], 
+      rides: [],
+      history: [],
+    }
+  
+    it("no lost data on post and get", function(done) {
+  
+      request(app)
+        .post("/users")
+        .send({
+          "_id": "123",
+          "full_name": "Joe Goldberg",
+          "given_name": "Joe", 
+          "family_name": "Goldberg", 
+          "email": "joegoldberg@ucsb.edu",
+          "drives": [], 
+          "rides": [],
+          "history": []
+        })
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(res => {
+          return request(app)
+            .get("/users")
+            .set('Accept', 'application/json')
+            .expect(200)
+        })
+        .then(res => {
+          console.log(body._id);
+          console.log(data._id);
+          const body = res.body[0]
+          assert(body._id === data._id);
+          assert(body.full_name === data.full_name);
+          assert(body.given_name === data.given_name);
+          assert(body.family_name === data.family_name);
+          assert(body.email === data.email);
+          assert(body.drives.length === 0);
+          assert(body.rides.length === 0);
+          assert(body.history.length === 0);
+        })
+        .then(res => {
+          done();
+        })
+        .catch(err => done('User already in database. \n[', error.message, ']\n'))
+    });
+  });
+  
