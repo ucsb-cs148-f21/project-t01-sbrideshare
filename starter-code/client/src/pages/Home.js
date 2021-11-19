4f589f4ce1260e91593f9ec25fda4b31f2ebe864
Heather Dinh <heatherdinh11@gmail.com>
2021-11-19 11:41:29 -0800

    a few UI changes again

--- starter-code/client/src/pages/Home.js
+++ starter-code/client/src/pages/Home.js
@@ -8,7 +8,6 @@ import { Carousel } from "react-bootstrap";
 import Layout from "../components/Layout";
 import getUser from "../utils/get-user";
 import getBackendURL from "../utils/get-backend-url";
-import CarImg from "../images/home_pic.png";
 import "../styles/home.css";
 
 import Card from '@mui/material/Card';
@@ -16,8 +15,6 @@ import CardContent from '@mui/material/CardContent';
 import { CardActionArea } from '@mui/material';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import mapache from '../images/mapache.png';
-import coconutPalm from '../images/coconut-palm-tree.png';
 
 const TextWrapper = styled.div`
   width: 700px;
@@ -50,17 +47,6 @@ export default function Home() {
         <div id="home-title">
             <h1>Welcome to SB RideShare!</h1>
         </div>
-
-        <div id="home-text">
-            {/*<TextWrapper>
-            <h5>A web application designed for UCSB students on the go.</h5>
-              Users can either browse current rides and join them as passengers, or
-              post one of their own rides as drivers that they are willing to take
-              passengers for.
-              <br />
-              Happy carpooling!
-            </TextWrapper>*/}
-        </div>
         <Carousel fade prevIcon="">
           
           <Carousel.Item>
@@ -83,18 +69,14 @@ export default function Home() {
                   <Typography variant="p" component="div"> 
                     Join a ride from another UCSB student.
                   </Typography>
-                  {//<img src={mapache} id="mapache-pic"/>
-                  }
                 </CardContent>
               </CardActionArea>
-          </Card>
-            <Carousel.Caption>
-              {/*<h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>*/}
-            </Carousel.Caption>
+            </Card>
             </div>
           </Carousel.Item>
+
           <Carousel.Item>
+            <div id="second-card">
             <Card id="right-card">
               <CardActionArea component={RouterLink} to="/ride">
                 <CardMedia
@@ -112,58 +94,12 @@ export default function Home() {
                   <Typography variant="p" component="div"> 
                     Drive on the platform with the largest network of UCSB students.
                   </Typography>
-                  {//<img src={coconutPalm} id="coconut-palm-pic"/>
-                  }
                 </CardContent>
               </CardActionArea>
             </Card>
-
-            <Carousel.Caption>
-              {/*<h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>*/}
-            </Carousel.Caption>
+            </div>
           </Carousel.Item>
         </Carousel>
-        {/*
-        <div className="card-row">
-          <div className="column">
-          <Card id="left-card">
-              <CardActionArea component={RouterLink} to="/rides">
-                <CardMedia
-                  component=""
-                  height="140"
-                  image=""
-                  alt=""
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Find A Ride
-                  </Typography>
-                  <img src={mapache} id="mapache-pic"/>
-                </CardContent>
-              </CardActionArea>
-          </Card>
-          </div>
-          
-          <div className="column">
-          <Card id="right-card">
-            <CardActionArea component={RouterLink} to="/ride">
-              <CardMedia
-                component=""
-                height="140"
-                image=""
-                alt=""
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Create A Ride
-                </Typography>
-                <img src={coconutPalm} id="coconut-palm-pic"/>
-              </CardContent>
-            </CardActionArea>
-           </Card>
-          </div>
-        </div> */}
       </Container>
     </Layout>
   );
